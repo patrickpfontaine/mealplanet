d5b20e6cedf4ffb5c1d475985ce463402afb038a
patrickpfontaine <patrick.herve.fontaine@gmail.com>
2024-11-06 00:09:16 -0600

    changed layout again

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,11 +13,18 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   const defaultScreenOptions = {
-    headerShown: false,
+    headerShown: false, // You can adjust this based on your needs
     contentStyle: {
-      backgroundColor: "#f3ede4",
+      backgroundColor: "#f3ede4", // Global background style for content
     },
   };
 
-  return <Stack screenOptions={defaultScreenOptions} />;
+  return (
+    <Stack screenOptions={defaultScreenOptions}>
+      <Stack.Screen name="SignIn" />
+      <Stack.Screen name="SignUp" />
+      {/* Add more screens here as your app grows */}
+    </Stack>
+  );
 }
+
